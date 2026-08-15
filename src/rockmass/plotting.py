52bea0b55abdd44c221aaa1d4c68ee88b683c1c3
codingMaz <m.balandari.toroghi@gmail.com>
2026-08-15 17:41:51 +0330

    add plotting structure

---
+++ src/rockmass/plotting.py
@@ -0,0 +1,15 @@
+"""
+plotting.py
+-----------
+Generates charts for RMR, Q-System, and GSI.
+Used in GUI and report generation.
+"""
+
+
+def create_rmr_bar_chart(breakdown: dict): ...
+
+
+def create_q_radar_chart(breakdown: dict): ...
+
+
+def create_gsi_diagram(breakdown: dict): ...
