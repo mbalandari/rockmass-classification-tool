de78069175d21f9964c02ae82a7b55153f855cfc
codingMaz <m.balandari.toroghi@gmail.com>
2026-08-15 17:37:55 +0330

    add gsi structure

---
+++ src/rockmass/gsi.py
@@ -0,0 +1,13 @@
+"""
+gsi.py
+------
+Implements Hoek–Marinos GSI system using lookup tables for structure and surface condition.
+"""
+
+
+def compute_gsi(input: GSIInput) -> (float, dict):
+    """
+    Compute GSI value and breakdown.
+    GSI = structure_base + surface_adjustment
+    """
+    ...
