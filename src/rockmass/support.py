6fcf74e022fd729b4f3e26e0ffde12141aa8f911
codingMaz <m.balandari.toroghi@gmail.com>
2026-08-15 17:38:51 +0330

    add support structure

---
+++ src/rockmass/support.py
@@ -0,0 +1,16 @@
+"""
+support.py
+----------
+Provides support recommendations based on Q-System value.
+"""
+
+
+def compute_support(q_value: float) -> dict:
+    """
+    Returns support recommendations:
+    - category
+    - bolt length
+    - bolt spacing
+    - shotcrete thickness
+    """
+    ...
